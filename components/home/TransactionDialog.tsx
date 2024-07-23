6f547da2fc9cfd3b616f027e5a256e2163c40c31
Siddesh Devarakonda <siddeshdslr@gmail.com>
2024-07-23 12:45:54 +0530

    add clear transaction feature

--- components/home/TransactionDialog.tsx
+++ components/home/TransactionDialog.tsx
@@ -1,4 +1,7 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import axios from "axios";
 import {
   Dialog,
   DialogContent,
@@ -16,6 +19,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { format } from "date-fns";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 interface Transaction {
   amount: number;
@@ -24,6 +28,7 @@ interface Transaction {
   first_name: string;
   last_name: string;
   email: string;
+  id: number;
 }
 
 interface Transactions {
@@ -37,6 +42,22 @@ interface Transactions {
 interface TransactionDialogProps {
   transactions: Transactions;
 }
+
+const UpdateCleared = (expenses_id: number) => {
+  axios
+    .patch(
+      "http://localhost:8080/home/clear",
+      { expenses_id: expenses_id },
+      { withCredentials: true }
+    )
+    .then((response) => {
+      console.log("Response:", response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Error in updating the transaction");
+    });
+};
 
 const TransactionDialog: React.FC<TransactionDialogProps> = ({
   transactions,
@@ -81,57 +102,70 @@ const TransactionDialog: React.FC<TransactionDialogProps> = ({
           )}
         </TableCell>
 
-        <DialogContent>
+        <DialogContent className="w-[40rem] h-fit max-h-[40rem] max-w-[38rem]">
           <DialogHeader>
             <DialogTitle>{transactions.name}</DialogTitle>
             <DialogDescription>
               Here is a detailed list of the payments and debts.
             </DialogDescription>
           </DialogHeader>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="text-center">Date</TableHead>
-                <TableHead className="text-center">Reason</TableHead>
-                <TableHead className="text-center">Amount</TableHead>
-                <TableHead className="hidden sm:table-cell text-center">
-                  Type
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {transactions_array.map((transaction, index) => (
-                <TableRow key={index}>
-                  <TableCell>
-                    <div className="font-medium text-center">
-                      {format(transaction.created_at, "PPP")}
-                    </div>
-                  </TableCell>
-                  <TableCell>
-                    <div className="font-medium text-center">
-                      {transaction.reason}
-                    </div>
-                  </TableCell>
-                  <TableCell className="text-right">
-                    ₹{Math.abs(transaction.amount).toFixed(2)}
-                  </TableCell>
-                  <TableCell className="hidden sm:table-cell text-center">
-                    {transaction.amount < 0 ? (
-                      <Badge className="text-xs" variant="outline">
-                        <span className="flex h-1.5 w-1.5 rounded-full bg-[#ED2B2A] mr-1.5"></span>
-                        Owe
-                      </Badge>
-                    ) : (
-                      <Badge className="text-xs" variant="secondary">
-                        <span className="flex h-1.5 w-1.5 rounded-full bg-green-600 mr-1.5"></span>
-                        Receive
-                      </Badge>
-                    )}
-                  </TableCell>
+          <ScrollArea className="h-full w-[36rem] rounded-md">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="text-center">Date</TableHead>
+                  <TableHead className="text-center">Reason</TableHead>
+                  <TableHead className="text-center">Amount</TableHead>
+                  <TableHead className="hidden sm:table-cell text-center">
+                    Type
+                  </TableHead>
+                  <TableHead className="text-center">Remove</TableHead>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHeader>
+              <TableBody>
+                {transactions_array.map((transaction, index) => (
+                  <TableRow key={index}>
+                    <TableCell>
+                      <div className="font-medium text-center">
+                        {format(transaction.created_at, "PPP")}
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="font-medium text-center">
+                        {transaction.reason}
+                      </div>
+                    </TableCell>
+                    <TableCell className="text-right">
+                      ₹{Math.abs(transaction.amount).toFixed(2)}
+                    </TableCell>
+                    <TableCell className="hidden sm:table-cell text-center">
+                      {transaction.amount < 0 ? (
+                        <Badge className="text-xs" variant="outline">
+                          <span className="flex h-1.5 w-1.5 rounded-full bg-[#ED2B2A] mr-1.5"></span>
+                          Owe
+                        </Badge>
+                      ) : (
+                        <Badge className="text-xs" variant="secondary">
+                          <span className="flex h-1.5 w-1.5 rounded-full bg-green-600 mr-1.5"></span>
+                          Receive
+                        </Badge>
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <DialogClose>
+                        <Button
+                          size="sm"
+                          onClick={() => UpdateCleared(transaction.id)}
+                        >
+                          Cleared
+                        </Button>
+                      </DialogClose>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </ScrollArea>
         </DialogContent>
       </Dialog>
     </>
--- components/home/Transactions.tsx
+++ components/home/Transactions.tsx
@@ -27,6 +27,7 @@ interface Transaction {
   first_name: string;
   last_name: string;
   email: string;
+  id: number;
 }
 
 interface Transactions {
